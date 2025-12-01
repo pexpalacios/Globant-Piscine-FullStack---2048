230cb9a10f4af82ab2623b0d6a4f9d29a15925b7
penpalac <penpalac@student.42madrid.com>
2025-12-01 12:51:23 +0100

    First push. HTML from subject, CSS half done

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+Methods to do:
+- Move up, move down, move left, move right
+- Combine numbers in case is viable
+- Event listener for arrow keys
+- Check win and game over
+- Restart game
+- Color tiles
+
+
+
+
+*/
